0b13b0e196d25824102e780f79055fd4efbcb6e0
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 10:39:46 -0500

    I have created the GetGamesUseCase class that returns a randomly ordered list of games.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/domain/GetGamesUseCase.kt
@@ -0,0 +1,19 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.domain
+
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.domain.item.GameItem
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.repo.GameRepository
+import javax.inject.Inject
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class GetGamesUseCase @Inject constructor(private val gameRepository: GameRepository) {
+
+    suspend operator fun invoke(): List<GameItem> {
+
+        return gameRepository.getGames().shuffled()
+
+    }
+
+}
