4a3eda9c49f054884846cc43c41dc2d5e3f03a3a
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 10:50:53 -0500

    I have created the HomeViewModel class that encapsulates the list of games obtained and exposes the state to the UI.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/ui/home/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.domain.GetGamesUseCase
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.domain.item.GameItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val getGamesUseCase: GetGamesUseCase) : ViewModel() {
+
+    private val _games = MutableStateFlow(emptyList<GameItem>())
+    val games: StateFlow<List<GameItem>> get() = _games
+
+    init {
+        getGames()
+    }
+
+    private fun getGames() {
+
+        viewModelScope.launch {
+
+            try {
+
+                val games = getGamesUseCase()
+                _games.value = games
+
+            } catch (_: Exception) {}
+
+        }
+
+    }
+
+}
