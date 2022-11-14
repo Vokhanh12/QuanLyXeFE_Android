45afa70a82861e12e4721bd2aaf06fa0c7f8754a
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 10:57:13 -0500

    I have created the Home File which is responsible for displaying a list of games on the screen.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/ui/home/Home.kt
@@ -0,0 +1,79 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.ui.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.domain.item.GameItem
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@Composable
+fun HomeScreen() {
+
+    val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
+    val games by homeViewModel.games.collectAsState()
+
+    LazyColumn {
+
+        items(games) { game: GameItem ->
+
+            GameCard(game = game)
+
+        }
+
+    }
+
+}
+
+@Composable
+fun GameCard(game: GameItem) {
+
+    val image = rememberImagePainter(data = game.thumbnail)
+
+    Card(
+
+        elevation = 5.dp,
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .padding(top = 5.dp, bottom = 5.dp, start = 10.dp, end = 10.dp)
+            .fillMaxSize()
+
+    ) {
+
+        Column {
+
+            Image(
+                painter = image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().height(250.dp)
+            )
+
+            Column(modifier = Modifier.padding(10.dp)) {
+
+                Text(text = game.title, fontWeight = FontWeight.Bold)
+                Text(text = game.short_description, maxLines = 2, overflow = TextOverflow.Ellipsis)
+
+            }
+
+        }
+
+    }
+
+}
