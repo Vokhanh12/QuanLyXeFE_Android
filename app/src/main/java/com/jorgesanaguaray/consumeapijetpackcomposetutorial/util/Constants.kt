b4c2158d93af8ee5a3402858aadf3a0eb7aa16f0
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 09:53:51 -0500

    I created the Constants class where I added the Base Url and its Endpoint.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.util
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://www.freetogame.com/api/"
+        const val GAMES_ENDPOINT = "games"
+
+    }
+
+}
