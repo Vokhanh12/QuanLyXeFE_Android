50405d8f6cf877701d71510a77eb73e44de569b8
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 09:50:57 -0500

    I created and added the class that extends Application to the Manifest, plus I added internet permissions.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/app/ConsumeApiJetpackComposeTutorial.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class ConsumeApiJetpackComposeTutorial : Application()
