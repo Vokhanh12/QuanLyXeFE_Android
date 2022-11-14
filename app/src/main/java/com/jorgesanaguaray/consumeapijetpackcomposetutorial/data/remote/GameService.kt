dfb3786568182ccb610e0e576dcac7ec34e4ceb9
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-14 10:28:52 -0500

    I have added GameService which returns a list of games.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposetutorial/data/remote/GameService.kt
@@ -0,0 +1,23 @@
+package com.jorgesanaguaray.consumeapijetpackcomposetutorial.data.remote
+
+import com.jorgesanaguaray.consumeapijetpackcomposetutorial.data.remote.model.GameModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class GameService @Inject constructor(private val gameApi: GameApi) {
+
+    suspend fun getGames(): List<GameModel> {
+
+        return withContext(Dispatchers.IO) {
+            val games = gameApi.getGames()
+            games.body() ?: emptyList()
+        }
+
+    }
+
+}
